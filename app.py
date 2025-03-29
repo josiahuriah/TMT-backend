60b8a8a71ca9ac04754baa4ec633f44a49d6e8ff
Josh <josh.duncanson@gmail.com>
2025-03-29 12:03:31 -0400

    Update port to match Render default

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5001))
+    port = int(os.getenv("PORT", 10000))  # Align with Render’s default
     debug = os.getenv("FLASK_DEBUG", "False") == "True"
     app.run(host="0.0.0.0", port=port, debug=debug)
