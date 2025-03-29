d6ba0ada226bebd719ce283c961eaa7f77a9b75b
Josh <josh.duncanson@gmail.com>
2025-03-29 12:20:25 -0400

    Added Cors

--- create_app.py
+++ create_app.py
@@ -8,6 +8,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     uri = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
