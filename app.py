6a02b54693ad03294f3af33d485e4b4235af1662
Josh <josh.duncanson@gmail.com>
2025-03-29 12:07:49 -0400

    Rollback

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))  # Align with Render’s default
+    port = int(os.getenv("PORT", 5001))
     debug = os.getenv("FLASK_DEBUG", "False") == "True"
     app.run(host="0.0.0.0", port=port, debug=debug)
